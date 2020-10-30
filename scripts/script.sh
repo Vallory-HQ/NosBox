ddf1d944a5e0772278985e00389778e32dd702bb
vallory-hq <vallory@nosotroshq.com>
2020-10-30 17:05:07 -0300

    NosBox v 1.3.4 release

--- scripts/script.sh
+++ scripts/script.sh
@@ -42,7 +42,7 @@ sudo apt-get install git
 git --version
 
 # Installing Composer
-
+sudo mkdir /home/vagrant/.composer
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === 'c31c1e292ad7be5f49291169c0ac8f683499edddcfd4e42232982d0fd193004208a58ff6f353fde0012d35fdd72bc394') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
