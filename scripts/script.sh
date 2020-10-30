d8757caf4550b85be86c20088dc1c98ebeb00c04
vallory-hq <vallory@nosotroshq.com>
2020-10-30 11:31:22 -0300

    NosBox v 1.1 release

--- scripts/script.sh
+++ scripts/script.sh
@@ -12,13 +12,13 @@ sudo apt-get -y install apache2
 
 sudo debconf-set-selections <<< 'mysql-server-5.7 mysql-server/root_password password secret'
 sudo debconf-set-selections <<< 'mysql-server-5.7 mysql-server/root_password_again password secret'
-sudo apt-get -y install mysql-server libapache2-mod-auth-mysql php7.4-mysql
+sudo apt-get -y install mysql-server php7.4-mysql
 
 # Installing PHP and it's dependencies
 sudo add-apt-repository ppa:ondrej/php -y 
 sudo apt-get update
 
-sudo apt-get -y install p7.4 php7.4-common php7.4-cli 
+sudo apt-get -y install php7.4 php7.4-common php7.4-cli 
 sudo apt-get -y install php7.4-bcmath php7.4-bz2 php7.4-curl php7.4-intl php7.4-mbstring php7.4-mysql php7.4-readline php7.4-xml php7.4-zip
 
 # Web Server Integration
@@ -27,3 +27,4 @@ sudo a2enconf php7.4-fpm # For Apache only
 
 sudo apt-get -y install libapache2-mod-php7.4
 
+echo vagrant | sudo systemctl reload apache2
